b3fb75eb0bb99c99c455ebb99ac1bf1a6b81852b
Superstar-Yang <Y1613912671@163.com>
2024-08-26 10:55:50 +0800

    test

--- react_basic/src/App.jsx
+++ react_basic/src/App.jsx
@@ -1,27 +1,11 @@
-import {useSelector, useDispatch} from "react-redux";
-import {increment, decrement,add} from "./store/modules/counter.js";
-import {useEffect} from "react";
-import {getChannels} from "./store/modules/channels.js";
 
 const App = () => {
-    const {count} = useSelector((state) => state.counter);
-    const {channels} = useSelector((state) => state.channel);
-    const dispatch = useDispatch();
-    useEffect(() => {
-      dispatch(getChannels())
-    },[dispatch])
+
     return (
         <div>
-            <button onClick={() => dispatch(increment())}>+</button>
-            <span>{count}</span>
-            <button onClick={() => dispatch(decrement())}>-</button>
-            <button onClick={() => dispatch(add(10))}>+10</button>
+
             <ul>
-                {
-                    channels.map(channel=>(
-                        <li key={channel.id}>{channel.name}</li>
-                    ))
-                }
+                hello
             </ul>
         </div>
     );
